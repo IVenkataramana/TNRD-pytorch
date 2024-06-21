f53d251fe5f3a613bf9f006426e382a92efdea02
IVenkataramana <122977472+IVenkataramana@users.noreply.github.com>
2024-06-22 00:01:39 +0530

    changes

--- denoising/data/__init__.py
+++ denoising/data/__init__.py
@@ -1,11 +1,11 @@
 import os
-from .datasets import  DatasetNoise
+from .datasets import DatasetNoise
 from torch.utils.data import DataLoader
 
 def get_loaders(args):
     # datasets
-    dataset_train = DatasetNoise(root=os.path.join(args.root, 'train'), noise_sigma=args.noise_sigma, training=True, crop_size=args.crop_size, blind_denoising=args.blind, gray_scale=args.gray_scale)
-    dataset_val = DatasetNoise(root=os.path.join(args.root, 'val'), noise_sigma=args.noise_sigma, training=False, crop_size=args.crop_size, blind_denoising=args.blind, gray_scale=args.gray_scale, max_size=args.max_size)
+    dataset_train = DatasetNoise(root='FoEtraining180', noise_sigma=args.noise_sigma, training=True, crop_size=args.crop_size, blind_denoising=args.blind, gray_scale=args.gray_scale)
+    dataset_val = DatasetNoise(root='FoEtraining180', noise_sigma=args.noise_sigma, training=False, crop_size=args.crop_size, blind_denoising=args.blind, gray_scale=args.gray_scale, max_size=args.max_size)
 
     # loaders
     loader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
